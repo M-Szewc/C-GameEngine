20860d5897fa7a9ac9307c6f47571e6af0f0bd71
M-Szewc <szewc18michal@gmail.com>
2025-10-03 01:49:20 +0200

    platform layer and entry point

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,11 +1,12 @@
 #include "logger.h"
+#include "asserts.h"
+#include "platform/platform.h"
 
 // TODO: remove temporary
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
-#include "asserts.h"
 
 b8 initialize_logging() {
     // TODO: create log file
@@ -19,23 +20,28 @@ void shutdown_logging() {
 
 void log_output(log_level level, const char* message, ...) {
     const char* level_strings[6] = {"[FATAL]:", "[ERROR]:", "[WARN]:", "[INFO]:", "[DEBUG]:", "[TRACE]:"};
-    //b8 is_error = level <= LOG_LEVEL_ERROR;
+    b8 is_error = level <= LOG_LEVEL_ERROR;
 
     // Technically imposes a 32k character limit on a single log entry, but no reasonable log is that long
-    char out_message[32000];
-    memset(out_message, 0, sizeof(out_message));
+    const i32 MAX_MSG_LENGTH = 32000;
+    char out_message[MAX_MSG_LENGTH];
+    platform_zero_memory(out_message, sizeof(out_message));
 
     // Format original message
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, 32000, message, arg_ptr);
+    vsnprintf(out_message, MAX_MSG_LENGTH, message, arg_ptr);
     va_end(arg_ptr);
 
-    char final_message[32000];
+    char final_message[MAX_MSG_LENGTH];
     sprintf(final_message, "%s %s\n", level_strings[level], out_message);
 
-    // TODO: platform-specific output
-    printf("%s", final_message);
+    // platform-specific output
+    if (is_error) {
+        platform_console_write_error(final_message, level);
+    } else {
+        platform_console_write(final_message, level);
+    }
 }
 
 void report_assertion_failure_msg(const char* expression, const char* message, const char* file, i32 line) {
---
+++ testbed/src/entry.c
@@ -0,0 +1,25 @@
+#include "game.h"
+
+#include <entry.h>
+
+// TODO: remove for platform alocation
+#include <stdlib.h>
+
+// Define the function to create a game
+b8 create_game(game* out_game) {
+
+    // Application configuration
+    out_game->app_config.start_pos_x = 100;
+    out_game->app_config.start_pos_y = 100;
+    out_game->app_config.start_width = 1280;
+    out_game->app_config.start_height = 720;
+    out_game->app_config.name = "Gearz Engine Testbed";
+    out_game->update = game_update;
+    out_game->render = game_render;
+    out_game->initialize = game_initialize;
+    out_game->on_resize = game_on_resize;
+    
+    out_game->state = malloc(sizeof(game_state));
+
+    return true;
+}
